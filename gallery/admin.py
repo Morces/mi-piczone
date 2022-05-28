745ed2ed3be9932819972a4e5fedf664eb289340
Moses Karani <karanim594@gmail.com>
2022-05-28 16:41:06 +0300

    added photos for diff locations and categories

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from gallery.models import Category, Image, Location
+
 # Register your models here.
+
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Location)
