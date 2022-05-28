7987f2a1e74d802490d33c21a422c76b435f61f3
Moses Karani <karanim594@gmail.com>
2022-05-28 20:52:35 +0300

    optimized landing page styles

--- gallery/views.py
+++ gallery/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
+from gallery.models import Image, Location
+
 
 
 # Create your views here.
 def index(request):
+    images = Image.get_all_images()
+    locations = Location.objects.all()
+    title = 'piczone'
 
     return render(request, 'gallery/index.html')
