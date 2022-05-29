85b091cf457d2b9136381db9427cf6e4d7226d18
Moses Karani <karanim594@gmail.com>
2022-05-29 10:50:55 +0300

    added urlpatterns - search&location,configured settings

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,8 +1,14 @@
+from django.conf import settings
 from django.urls import re_path as url
 from . import views
-
+from django.conf.urls.static import static
 
 urlpatterns = [
     url (r'^$', views.index, name='index'),
-    url(r'^image/(?P<category_name>\w+)/(?P<image_id>\d+)', views.single, name='single')
+    url(r'^image/(?P<category_name>\w+)/(?P<image_id>\d+)', views.single, name='single'),
+    url(r'^location/(?P<image_location>\d+)', views.location_filter, name = 'location_filter'),
+    url(r'^search/', views.search_image, name='search_image')
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
